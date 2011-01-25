412d7e55eb00d174956a2b62dfd624bd0111d484
Phil Edworthy <phil.edworthy@renesas.com>
2011-01-25 11:46:39 +0000

    Tools: Match colorspace & size exactly

--- src/tools/shbeu-display.c
+++ src/tools/shbeu-display.c
@@ -118,7 +118,7 @@ static int set_size (char * arg, int * w, int * h)
 		return -1;
 
 	for (i=0; i<nr_sizes; i++) {
-		if (!strncasecmp (arg, sizes[i].name, strlen(sizes[i].name))) {
+		if (!strcasecmp (arg, sizes[i].name)) {
 			*w = sizes[i].w;
 			*h = sizes[i].h;
 			return 0;
@@ -177,7 +177,7 @@ static int set_colorspace (char * arg, ren_vid_format_t * c, int * is_bmp)
 		*is_bmp = 1;
 
 	for (i=0; i<nr_exts; i++) {
-		if (!strncasecmp (arg, exts[i].ext, strlen(exts[i].ext))) {
+		if (!strcasecmp (arg, exts[i].ext)) {
 			*c = exts[i].fmt;
 			return 0;
 		}
